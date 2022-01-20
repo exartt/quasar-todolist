63c561adcb303c76e8300e09d01c32d1d92d9bb3
Leonardo Moraes <lmoraes1644cadastros@gmail.com>
2022-01-19 22:49:27 -0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column" style="max-width: 300px" v-for="tarefa in tarefas" :key="tarefa.text">
+      <q-input clearable filled color="purple-12" v-model="tarefa.text" label="Label" />
+      <q-input
+        clearable
+        clear-icon="close"
+        filled
+        color="purple-12"
+        v-model="tarefa.text"
+        label="Label"
+      />
+    </div>
+    <div class="q-gutter-md" style="max-width: 500px">
+      <q-input outlined v-model="novaTarefa" label="Descrição" />
+      <q-btn
+        @click="gerarTarefa"
+        v-bind="size" :key="`btn_size_round_${size}`"
+        round
+        color="primary"
+        :size="size"
+        :icon="icon"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      novaTarefa: '',
+      tarefas: [],
+      size: 'md',
+      icon: 'add_a_photo'
+    }
+  },
+  methods: {
+    gerarTarefa () {
+      const index = this.tarefas.findIndex(tarefa => tarefa.text === this.novaTarefa)
+      if (index === -1) {
+        this.tarefas.push({
+          text: this.novaTarefa,
+          checked: false
+        })
+      } this.novaTarefa = ''
+    }
+  }
+}
+</script>
